0b4f6761c18043e30dd49e6fbae4ec40c5212f8d
alectramell <alectramell@gmail.com>
2015-03-14 13:54:12 -0700

    DIAMOND

--- __main__.py
+++ __main__.py
@@ -2,99 +2,98 @@
 
 import os
 import getpass
-import time
-
-os.system('clear')
 
 username = getpass.getuser()
 username = str(username)
 
-filename = raw_input('filename: ')
-filename = str(filename)
+dlink = str('''
+[Desktop Entry]
+Name=Create Executable
+Comment=Diamond Python Encode Utility
+Categories=GNOME;Utility;
+Exec=python /home/''' + username + '''/diamond/create-executable.py
+Icon=/home/''' + username + '''/diamond/icons/ubuntu64x64.png
+Terminal=false
+Type=Application
+''')
 
-intr = str('#!/usr/bin/python\n')
-spce = str('# (c) 2015 | Diamond Code Conversion for Python Libraries | Alec Tramell, DMA | alectramell@gmail.com')
-info = str('''# [?] use >> from ''' + filename + ''' import * << to import this file ;)\n''')
-defn = str('''def diamond():''')
-
-os.system('echo "' + intr + '" > /home/' + username + '/Desktop/' + filename + '.py')
-os.system('echo "' + spce + '" >> /home/' + username + '/Desktop/' + filename + '.py')
-os.system('echo "' + info + '" >> /home/' + username + '/Desktop/' + filename + '.py')
-os.system('echo "' + defn + '" >> /home/' + username + '/Desktop/' + filename + '.py')
-
-os.system('clear')
-
-word = raw_input('Assign variables (use plain text alphabet, a-z, lower-case): ')
-word = str(word)
-for char in word:
-    if char == ('a'):
-       char = str('''    a = str('s')''')
-    if char == ('b'):
-       char = str('''    b = str('a')''')
-    if char == ('c'):
-       char = str('''    c = str('p')''')
-    if char == ('d'):
-       char = str('''    d = str('h')''')
-    if char == ('e'):
-       char = str('''    e = str('i')''')
-    if char == ('f'):
-       char = str('''    f = str('r')''')
-    if char == ('g'):
-       char = str('''    g = str('e')''')
-    if char == ('h'):
-       char = str('''    h = str('b')''')
-    if char == ('i'):
-       char = str('''    i = str('c')''')
-    if char == ('j'):
-       char = str('''    j = str('d')''')
-    if char == ('k'):
-       char = str('''    k = str('f')''')
-    if char == ('l'):
-       char = str('''    l = str('g')''')
-    if char == ('m'):
-       char = str('''    m = str('j')''')
-    if char == ('n'):
-       char = str('''    n = str('k')''')
-    if char == ('o'):
-       char = str('''    o = str('l')''')
-    if char == ('p'):
-       char = str('''    p = str('m')''')
-    if char == ('q'):
-       char = str('''    q = str('n')''')
-    if char == ('r'):
-       char = str('''    r = str('o')''')
-    if char == ('s'):
-       char = str('''    s = str('q')''')
-    if char == ('t'):
-       char = str('''    t = str('t')''')
-    if char == ('u'):
-       char = str('''    u = str('u')''')
-    if char == ('v'):
-       char = str('''    v = str('v')''')
-    if char == ('w'):
-       char = str('''    w = str('w')''')
-    if char == ('x'):
-       char = str('''    x = str('x')''')
-    if char == ('y'):
-       char = str('''    y = str('y')''')
-    if char == ('z'):
-       char = str('''    z = str('z')''')
-    os.system('echo "' + char + '" >> /home/' + username + '/Desktop/' + filename + '.py')
-    time.sleep(.25)
-
-xcom = list(word)
-dcom = ' + '.join(xcom)
-dcom = str(dcom)
+mkshortcut = str('echo "' + dlink + '" > /home/' + username + '/diamond/create-executable.desktop')
+actshort = str('chmod +x /home/' + username + '/diamond/create-executable.desktop')
+os.system(mkshortcut)
+os.system(actshort)
 
 os.system('clear')
 
-os.system('echo "    print (' + dcom + ')" >> /home/' + username + '/Desktop/' + filename + '.py')
+word = raw_input("Translate DIAMOND to TEXT: ")
+word = list(word)
 
-os.system('echo "\ndiamond()" >> /home/' + username + '/Desktop/' + filename + '.py')
+output = ' + '.join(word)
+output = str(output)
 
-os.system('gedit /home/' + username + '/Desktop/' + filename + '.py &')
+text = str('print (' + output + ')')
+
+document = str('''
+tlib = str('abcdefghijklmnopqrstuvwxyz')
+tlib = list(tlib)
+dlib = str('saphirebcdfgjklmnoqtuvwxyz')
+dlib = list(dlib)
+space = (' ')
+
+N = str(space)
+s = str(tlib[0])
+a = str(tlib[1])
+p = str(tlib[2])
+h = str(tlib[3])
+i = str(tlib[4])
+r = str(tlib[5])
+e = str(tlib[6])
+b = str(tlib[7])
+c = str(tlib[8])
+d = str(tlib[9])
+f = str(tlib[10])
+g = str(tlib[11])
+j = str(tlib[12])
+k = str(tlib[13])
+l = str(tlib[14])
+m = str(tlib[15])
+n = str(tlib[16])
+o = str(tlib[17])
+q = str(tlib[18])
+t = str(tlib[19]) 
+u = str(tlib[20])
+v = str(tlib[21])
+w = str(tlib[22])
+x = str(tlib[23])
+y = str(tlib[24])
+z = str(tlib[25])
+
+''' + text + '''
+
+''')
+
+com1 = str('echo "#!/usr/bin/python" > ~/Desktop/code.py')
+com2 = str('echo "' + document + '" >> ~/Desktop/code.py')
+
+os.system(com1)
+os.system(com2)
+
+os.system('python ~/Desktop/code.py > ~/Desktop/.dmsg && rm -r ~/Desktop/code.py && clear')
+
+os.system('touch ~/Desktop/.diamond-msg.sh')
+
+os.system('echo "#!/bin/bash\n" > ~/Desktop/.diamond-msg.sh')
+os.system('echo "clear\n" >> ~/Desktop/.diamond-msg.sh')
+os.system('echo "cat ~/Desktop/.dmsg && read && clear\n" >> ~/Desktop/.diamond-msg.sh')
+
+os.system('chmod +x ~/Desktop/.diamond-msg.sh')
+
+os.system('gnome-terminal --title="..Diamond | Translation | press [ENTER] to exit.." -x sh -c "~/Desktop/./.diamond-msg.sh && rm -r ~/Desktop/.dmsg && rm -r ~/Desktop/.diamond-msg.sh"')
 
 os.system('clear')
+
+
+
+
 
 
 
